9b4885094e12d95587e997b2a4a48e0843d8c9be
fyull34 <m.winski.219@studms.ug.edu.pl>
2026-07-01 18:08:12 +0200

    first_board_implementation

--- board.py
+++ board.py
@@ -1,18 +1,39 @@
 import pygame
 
 pygame.init()
-screen = pygame.display.set_mode((800, 800))
+screen = pygame.display.set_mode((1200, 1200))
 clock = pygame.time.Clock()
 running = True
+
+tiles = []
+tile = pygame.Rect(0,0,120,120)
+
+for i in range(8):
+    for j in range(8):
+        if (i + j) % 2 == 0:
+            tiles.append('0')
+        else:
+            tiles.append('1')
+    
 
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            running = False
+            running = False    
 
     screen.fill("white")
 
-    pygame.draw.rect(screen, "red", (50,100,50,50), width=0, border_radius=0)
+    x = 0
+    y = 0
+
+    for i in tiles:
+        if i == '1':
+            pygame.draw.rect(screen, "gray", (x, y, 150, 150))
+
+        x += 150
+        if x == 8 * 150:
+            x = 0
+            y += 150
 
     pygame.display.flip()
 
